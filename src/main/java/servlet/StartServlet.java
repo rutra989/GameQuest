19c811801548d6f1aef9455ab63213b2c4a23ea6
Denis <rutra989@mail.ru>
2026-08-08 22:24:51 +0300

    init: добавил логику методу  doPost и doGet в StartServlet feat: создал класс QuestLoader, QuestRepository и папку repository

--- src/main/java/servlet/StartServlet.java
+++ src/main/java/servlet/StartServlet.java
@@ -1,11 +1,15 @@
 package servlet;
 
+import model.GameState;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.Random;
 
 @WebServlet("/start")
 public class StartServlet extends HttpServlet {
@@ -15,7 +19,13 @@ public class StartServlet extends HttpServlet {
     }
 
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        super.doPost(req, resp);
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+        HttpSession session = req.getSession();
+        GameState gm = new GameState();
+        gm.setPlayerName(req.getParameter("username"));
+        gm.setPlayerId(new Random().nextInt(10000));
+        gm.setCurrentStepId(1);
+        session.setAttribute("gameState",gm);
+        resp.sendRedirect("/game");
     }
 }
