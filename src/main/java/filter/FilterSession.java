3d566f092088bd0f22e0e7b93557c974a76fb130
Denis <rutra989@mail.ru>
2026-08-08 15:12:34 +0300

    init: add filter classes

---
+++ src/main/java/filter/FilterSession.java
@@ -0,0 +1,11 @@
+package filter;
+
+import javax.servlet.*;
+import java.io.IOException;
+
+public class FilterSession implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
