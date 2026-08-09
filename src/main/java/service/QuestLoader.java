7d5ad5e0f338b6107d5004bf1f9e2cbeed086641
Denis <rutra989@mail.ru>
2026-08-09 19:37:30 +0300

    init: написал логику чтения файла json в классе QuestLoader init: написал логику чтения пути к файлу json в классе GameServlet

--- src/main/java/service/QuestLoader.java
+++ src/main/java/service/QuestLoader.java
@@ -1,4 +1,18 @@
 package service;
 
+import model.Step;
+import tools.jackson.core.type.TypeReference;
+import tools.jackson.databind.ObjectMapper;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
 public class QuestLoader {
+    ObjectMapper objectMapper = new ObjectMapper();
+    public List<Step> load(String path) throws IOException {
+        File file = new File(path);
+        return objectMapper.readValue(file,new TypeReference<List<Step>>(){});
+    }
+
 }
--- src/main/java/servlet/GameServlet.java
+++ src/main/java/servlet/GameServlet.java
@@ -1,14 +1,31 @@
 package servlet;
 
+import model.Step;
+import service.QuestLoader;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet("/game")
 public class GameServlet  extends HttpServlet {
+
+    @Override
+    public void init() throws ServletException {
+        super.init();
+        String path = getServletContext().getRealPath("/WEB-INF/data/quest.json");
+        QuestLoader loader = new QuestLoader();
+        try {
+            List<Step> steps = loader.load(path);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         super.doGet(req, resp);
